cddac48cbed962c52e4a7d11ce7620b02c12c4e5
José Alberto <contato@josealbertonp.com>
2023-03-21 23:50:18 -0300

    Delete api-nobanco directory

--- api-nobanco/meu_aplicativo/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MeuAplicativoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'meu_aplicativo'
--- api-nobanco/meu_aplicativo/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-from .module.usuario.usuario import Usuario
-from .module.contaGeral.contaGeral import ContaGeral
-from .module.saldo.saldo import Saldo
-from .module.compra.compra import Compra
-
-__all__ = ["Usuario", "ContaGeral", "Saldo", "Compra"]
